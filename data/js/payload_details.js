5e0c669c0ac888cee54818923ae8e83f457185e2
Keith Frankish <k.frankish@gmail.com>
2020-06-23 05:12:18 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 59020,
-	"created_at": "Sun Jun 21 19:57:46 +0000 2020"
+	"tweets": 59021,
+	"created_at": "Tue Jun 23 02:12:17 +0000 2020"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 22 19:35:23 +0000 2020",
+		"id": 1275150411494809600,
+		"id_str": "1275150411494809605",
+		"full_text": "Universities will never be the same after the coronavirus crisis - https://t.co/S2YZnmVjsI on @nature",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nature",
+					"name": "Nature",
+					"id": 487833518,
+					"id_str": "487833518",
+					"indices": [
+						94,
+						101
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/S2YZnmVjsI",
+					"expanded_url": "https://www.nature.com/articles/d41586-020-01518-y",
+					"display_url": "nature.com/articles/d4158…",
+					"indices": [
+						67,
+						90
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 52522040,
+			"id_str": "52522040",
+			"name": "Keith Frankish",
+			"screen_name": "keithfrankish",
+			"location": "Heraklion, Crete, Greece",
+			"description": "Philosopher & writer. European. Honorary Reader @sheffielduni. Interests in consciousness, reasoning, belief. Tweeting phil mind, cog sci, Greece, links & chat.",
+			"url": "http://t.co/tXhT44huzC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/tXhT44huzC",
+							"expanded_url": "http://www.keithfrankish.com",
+							"display_url": "keithfrankish.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13208,
+			"friends_count": 2073,
+			"listed_count": 485,
+			"created_at": "Tue Jun 30 21:10:19 +0000 2009",
+			"favourites_count": 37617,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 57936,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "304075",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1207775707839041538/osgc7L9X_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1207775707839041538/osgc7L9X_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/52522040/1554843010",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "C0C6CC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Universities will never be the same after the coronavirus crisis - https://t.co/S2YZnmVjsI on @nature"
+	},
+	{
 		"created_at": "Sun Jun 21 18:54:27 +0000 2020",
 		"id": 1274777722536046600,
 		"id_str": "1274777722536046592",
@@ -8870,7 +8987,7 @@ Grailbird.data.tweets_2020_06 =
 			"user_mentions": [
 				{
 					"screen_name": "jmattocks",
-					"name": "Jeremy 🧙‍♂️❤️🌯",
+					"name": "Jeremy 🧙♂️❤️🌯",
 					"id": 10866452,
 					"id_str": "10866452",
 					"indices": [
@@ -53125,7 +53242,7 @@ Grailbird.data.tweets_2020_06 =
 			"user_mentions": [
 				{
 					"screen_name": "fabrizioviani",
-					"name": "ｆａｂｒｉｚｉｏ ​🚴🔬📚",
+					"name": "ｆａｂｒｉｚｉｏ 🚴🔬📚",
 					"id": 222437713,
 					"id_str": "222437713",
 					"indices": [
@@ -53654,7 +53771,7 @@ Grailbird.data.tweets_2020_06 =
 			"user_mentions": [
 				{
 					"screen_name": "fabrizioviani",
-					"name": "ｆａｂｒｉｚｉｏ ​🚴🔬📚",
+					"name": "ｆａｂｒｉｚｉｏ 🚴🔬📚",
 					"id": 222437713,
 					"id_str": "222437713",
 					"indices": [
