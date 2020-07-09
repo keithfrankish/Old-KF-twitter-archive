d59d7faf7d36d2c27452d9e4332a8377134dd9de
Keith Frankish <k.frankish@gmail.com>
2020-07-09 05:12:15 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 59336,
-	"created_at": "Wed Jul 08 02:12:13 +0000 2020"
+	"tweets": 59337,
+	"created_at": "Thu Jul 09 02:12:14 +0000 2020"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Wed Jul 08 18:45:09 +0000 2020",
+		"id": 1280935975938654200,
+		"id_str": "1280935975938654209",
+		"full_text": "Johnny Cash - The first time ever I saw your face - YouTube - https://t.co/039Xa8AvkS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			85
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/039Xa8AvkS",
+					"expanded_url": "https://www.youtube.com/watch?v=EdSIlVZhsDw",
+					"display_url": "youtube.com/watch?v=EdSIlV…",
+					"indices": [
+						62,
+						85
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 52522040,
+			"id_str": "52522040",
+			"name": "Keith Frankish",
+			"screen_name": "keithfrankish",
+			"location": "Heraklion, Crete, Greece",
+			"description": "Philosopher & writer. European. Honorary Reader @sheffielduni. Interests in consciousness, reasoning, belief. Tweeting phil mind, cog sci, Greece, links & chat.",
+			"url": "http://t.co/tXhT44huzC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/tXhT44huzC",
+							"expanded_url": "http://www.keithfrankish.com",
+							"display_url": "keithfrankish.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13368,
+			"friends_count": 2122,
+			"listed_count": 486,
+			"created_at": "Tue Jun 30 21:10:19 +0000 2009",
+			"favourites_count": 37931,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 58248,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "304075",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1207775707839041538/osgc7L9X_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1207775707839041538/osgc7L9X_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/52522040/1554843010",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "C0C6CC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Johnny Cash - The first time ever I saw your face - YouTube - https://t.co/039Xa8AvkS"
+	},
+	{
 		"created_at": "Wed Jul 08 02:06:25 +0000 2020",
 		"id": 1280684634926395400,
 		"id_str": "1280684634926395392",
@@ -3313,7 +3419,7 @@ Grailbird.data.tweets_2020_07 =
 			"user_mentions": [
 				{
 					"screen_name": "longbulusiboshi",
-					"name": "Cancellist about Duffholes 🤦‍♂️🛰☞🌌b🌧 ☞ 🛰🤦‍♂️",
+					"name": "Cancellist about Duffholes 🤦♂️🛰☞🌌b🌧 ☞ 🛰🤦♂️",
 					"id": 1197860197714153500,
 					"id_str": "1197860197714153473",
 					"indices": [
@@ -4270,7 +4376,7 @@ Grailbird.data.tweets_2020_07 =
 			"user_mentions": [
 				{
 					"screen_name": "longbulusiboshi",
-					"name": "Cancellist about Duffholes 🤦‍♂️🛰☞🌌b🌧 ☞ 🛰🤦‍♂️",
+					"name": "Cancellist about Duffholes 🤦♂️🛰☞🌌b🌧 ☞ 🛰🤦♂️",
 					"id": 1197860197714153500,
 					"id_str": "1197860197714153473",
 					"indices": [
