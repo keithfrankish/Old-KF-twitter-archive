991203ca6b7237f07da3ae0ee65e09fcf5b31b4e
Keith Frankish <k.frankish@gmail.com>
2021-02-11 04:12:15 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 64074,
-	"created_at": "Thu Feb 11 00:54:37 +0000 2021"
+	"tweets": 64075,
+	"created_at": "Thu Feb 11 02:12:14 +0000 2021"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Thu Feb 11 01:50:32 +0000 2021",
+		"id": 1359681194019938300,
+		"id_str": "1359681194019938311",
+		"full_text": "@epicciuto @XLilliputian @evil_diad Not meaning to challenge you or your sentiment (with which as close relative of a nonverbal adult, I totally agree), but did Sh mean a nonverbal intellectually disabled person? An online Sh glossary gives \nidiot (n.) jester, clown, fool--and 'tale told by' suggests verbal ability",
+		"truncated": false,
+		"display_text_range": [
+			36,
+			316
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "epicciuto",
+					"name": "Elizabeth Picciuto 🤞🌨",
+					"id": 36651871,
+					"id_str": "36651871",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "XLilliputian",
+					"name": "LilliputianX",
+					"id": 1299796639884996600,
+					"id_str": "1299796639884996608",
+					"indices": [
+						11,
+						24
+					]
+				},
+				{
+					"screen_name": "evil_diad",
+					"name": "David 🌐",
+					"id": 798903192889212900,
+					"id_str": "798903192889212928",
+					"indices": [
+						25,
+						35
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1359675391816867800,
+		"in_reply_to_status_id_str": "1359675391816867842",
+		"in_reply_to_user_id": 36651871,
+		"in_reply_to_user_id_str": "36651871",
+		"in_reply_to_screen_name": "epicciuto",
+		"user": {
+			"id": 52522040,
+			"id_str": "52522040",
+			"name": "Keith Frankish",
+			"screen_name": "keithfrankish",
+			"location": "Heraklion, Crete, Greece",
+			"description": "Philosopher & writer. European. Honorary Reader @sheffielduni. Interests in consciousness, reasoning, belief. Tweeting phil mind, cog sci, Greece, links & chat.",
+			"url": "http://t.co/tXhT44huzC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/tXhT44huzC",
+							"expanded_url": "http://www.keithfrankish.com",
+							"display_url": "keithfrankish.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14966,
+			"friends_count": 2210,
+			"listed_count": 505,
+			"created_at": "Tue Jun 30 21:10:19 +0000 2009",
+			"favourites_count": 43372,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 63063,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "304075",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1207775707839041538/osgc7L9X_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1207775707839041538/osgc7L9X_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/52522040/1554843010",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "C0C6CC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@epicciuto @XLilliputian @evil_diad Not meaning to challenge you or your sentiment (with which as close relative of a nonverbal adult, I totally agree), but did Sh mean a nonverbal intellectually disabled person? An online Sh glossary gives \nidiot (n.) jester, clown, fool--and 'tale told by' suggests verbal ability"
+	},
+	{
 		"created_at": "Thu Feb 11 00:35:57 +0000 2021",
 		"id": 1359662421279916000,
 		"id_str": "1359662421279916034",
