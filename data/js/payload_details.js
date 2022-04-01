c18e5adf601a28b34e453dc478fbfae4192d33f5
Keith Frankish <k.frankish@gmail.com>
2022-04-01 05:12:11 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 70511,
-	"created_at": "Fri Apr 01 00:54:39 +0000 2022"
+	"tweets": 70512,
+	"created_at": "Fri Apr 01 02:12:10 +0000 2022"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,173 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Fri Apr 01 01:12:37 +0000 2022",
+		"id": 1509700226466168800,
+		"id_str": "1509700226466168833",
+		"full_text": "@_xiaoyangyu @chrisfcarroll @memneon @BugRib @PhilipVukovic @Philip_Goff It's the other way round\nreal magic = stage magic\nReal Magic = supernatural magic",
+		"truncated": false,
+		"display_text_range": [
+			73,
+			154
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "_xiaoyangyu",
+					"name": "Xiaoyang Yu",
+					"id": 51971148,
+					"id_str": "51971148",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "chrisfcarroll",
+					"name": "Chris F. Carroll",
+					"id": 92929966,
+					"id_str": "92929966",
+					"indices": [
+						13,
+						27
+					]
+				},
+				{
+					"screen_name": "memneon",
+					"name": "memneon 💙",
+					"id": 275985715,
+					"id_str": "275985715",
+					"indices": [
+						28,
+						36
+					]
+				},
+				{
+					"screen_name": "BugRib",
+					"name": "Your Everyday Anti-Physicalist Atheist",
+					"id": 2903956134,
+					"id_str": "2903956134",
+					"indices": [
+						37,
+						44
+					]
+				},
+				{
+					"screen_name": "PhilipVukovic",
+					"name": "Philip Vukovic",
+					"id": 1466155594398769200,
+					"id_str": "1466155594398769158",
+					"indices": [
+						45,
+						59
+					]
+				},
+				{
+					"screen_name": "Philip_Goff",
+					"name": "Philip Goff",
+					"id": 216809385,
+					"id_str": "216809385",
+					"indices": [
+						60,
+						72
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1509697670210031600,
+		"in_reply_to_status_id_str": "1509697670210031617",
+		"in_reply_to_user_id": 51971148,
+		"in_reply_to_user_id_str": "51971148",
+		"in_reply_to_screen_name": "_xiaoyangyu",
+		"user": {
+			"id": 52522040,
+			"id_str": "52522040",
+			"name": "Keith Frankish",
+			"screen_name": "keithfrankish",
+			"location": "Heraklion, Crete, Greece",
+			"description": "Philosopher, writer, Ελληνοβρετανός. Hon Reader @sheffielduni. Mind, consciousness, illusionism, cog-sci, Ελλάδα. Podcast: https://t.co/kyMR0mRBqm",
+			"url": "https://t.co/B6jDJI5AoE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/B6jDJI5AoE",
+							"expanded_url": "https://www.keithfrankish.com",
+							"display_url": "keithfrankish.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/kyMR0mRBqm",
+							"expanded_url": "http://youtube.com/c/MindChat",
+							"display_url": "youtube.com/c/MindChat",
+							"indices": [
+								123,
+								146
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 20434,
+			"friends_count": 2338,
+			"listed_count": 558,
+			"created_at": "Tue Jun 30 21:10:19 +0000 2009",
+			"favourites_count": 47966,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 69697,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "304075",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1476144027267387393/oJfSUngb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1476144027267387393/oJfSUngb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/52522040/1644622839",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "C0C6CC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@_xiaoyangyu @chrisfcarroll @memneon @BugRib @PhilipVukovic @Philip_Goff It's the other way round\nreal magic = stage magic\nReal Magic = supernatural magic"
+	},
+	{
 		"created_at": "Fri Apr 01 00:47:33 +0000 2022",
 		"id": 1509693917658292200,
 		"id_str": "1509693917658292224",
