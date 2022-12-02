e763fef4a06e3bd716dd83aeefeb67103670f0c6
Keith Frankish <k.frankish@gmail.com>
2022-12-02 02:54:39 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,4 +1,4 @@
 var payload_details = {
-	"tweets": 75130,
-	"created_at": "Thu Dec 01 00:54:46 +0000 2022"
+	"tweets": 75132,
+	"created_at": "Fri Dec 02 00:54:38 +0000 2022"
 }
---
+++ data/js/tweets/2022_12.js
@@ -0,0 +1,310 @@
+Grailbird.data.tweets_2022_12 = 
+[
+	{
+		"created_at": "Thu Dec 01 16:04:40 +0000 2022",
+		"id": 1598347359007694800,
+		"id_str": "1598347359007694853",
+		"full_text": "@martinjanello If I'm one of the jerks you have in mind, I apologize. I know I don't always reply to comments. No rudeness intended; I'm just overwhelmed",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			153
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "martinjanello",
+					"name": "Martin Janello",
+					"id": 2681206494,
+					"id_str": "2681206494",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1598143861779730400,
+		"in_reply_to_status_id_str": "1598143861779730436",
+		"in_reply_to_user_id": 2681206494,
+		"in_reply_to_user_id_str": "2681206494",
+		"in_reply_to_screen_name": "martinjanello",
+		"user": {
+			"id": 52522040,
+			"id_str": "52522040",
+			"name": "Keith Frankish (@keithfrankish@mastodon.social)",
+			"screen_name": "keithfrankish",
+			"location": "Heraklion, Crete, Greece",
+			"description": "Philosopher, writer, Ελληνοβρετανός. Hon Professor @sheffielduni. Mind, consciousness, illusionism, cog-sci, Ελλάδα. Podcast: https://t.co/kyMR0mRBqm",
+			"url": "https://t.co/B6jDJI5AoE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/B6jDJI5AoE",
+							"expanded_url": "https://www.keithfrankish.com",
+							"display_url": "keithfrankish.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/kyMR0mRBqm",
+							"expanded_url": "http://youtube.com/c/MindChat",
+							"display_url": "youtube.com/c/MindChat",
+							"indices": [
+								126,
+								149
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 24522,
+			"friends_count": 2381,
+			"listed_count": 585,
+			"created_at": "Tue Jun 30 21:10:19 +0000 2009",
+			"favourites_count": 50988,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 74446,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "304075",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1527010620079493122/d61-0NYu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1527010620079493122/d61-0NYu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/52522040/1668875289",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "C0C6CC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@martinjanello If I'm one of the jerks you have in mind, I apologize. I know I don't always reply to comments. No rudeness intended; I'm just overwhelmed"
+	},
+	{
+		"created_at": "Thu Dec 01 13:46:03 +0000 2022",
+		"id": 1598045384210894800,
+		"id_str": "1598312474704486403",
+		"full_text": "Heraklion\n1965-04-03\nIraklion, Crete,  Greece\n\n#Greece #Crete https://t.co/9BSEJOKrHS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			61
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Greece",
+					"indices": [
+						47,
+						54
+					]
+				},
+				{
+					"text": "Crete",
+					"indices": [
+						55,
+						61
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1598045379462893600,
+					"id_str": "1598045379462893577",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi1lzknXEAklCBM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi1lzknXEAklCBM.jpg",
+					"url": "https://t.co/9BSEJOKrHS",
+					"display_url": "pic.twitter.com/9BSEJOKrHS",
+					"expanded_url": "https://twitter.com/50sPhotosBot/status/1598045384210894848/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1407,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 824,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 467,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1598045379462893600,
+					"id_str": "1598045379462893577",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi1lzknXEAklCBM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi1lzknXEAklCBM.jpg",
+					"url": "https://t.co/9BSEJOKrHS",
+					"display_url": "pic.twitter.com/9BSEJOKrHS",
+					"expanded_url": "https://twitter.com/50sPhotosBot/status/1598045384210894848/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1407,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 824,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 467,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://567565.xyz/\" rel=\"nofollow\">Old Photographs Bot</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1579766559077273600,
+			"id_str": "1579766559077273600",
+			"name": "Old Photographs Bot",
+			"screen_name": "50sPhotosBot",
+			"location": "the cloud maaaan",
+			"description": "Posting pictures from a bygone era (mostly the 1950s and 60s)\nMom @flibblemeister",
+			"url": "https://t.co/S0x2CDHdM7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/S0x2CDHdM7",
+							"expanded_url": "https://github.com/flibble-err/OldPicsBot",
+							"display_url": "github.com/flibble-err/Ol…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21,
+			"friends_count": 2,
+			"listed_count": 0,
+			"created_at": "Tue Oct 11 09:31:26 +0000 2022",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2381,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1579952254248951826/3MNcJGjA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1579952254248951826/3MNcJGjA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1579766559077273600/1665524024",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "es",
+		"text": "Heraklion\n1965-04-03\nIraklion, Crete,  Greece\n\n#Greece #Crete https://t.co/9BSEJOKrHS"
+	}
+]
